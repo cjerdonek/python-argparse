0ff26f741d1f8c5c31dd9626924fee1f037ae7af
Chris Jerdonek <chris.jerdonek@gmail.com>
2014-12-21 13:40:57 -0800

    Stub out test_argparse2.py.

---
+++ argparse2/test/test_argparse2.py
@@ -0,0 +1,15 @@
+
+"""Contains tests added by the argparse2 project."""
+
+import unittest
+
+from argparse2.test import support as ap2_support
+
+argparse = ap2_support.get_argparse_under_test()
+
+
+# TODO
+class Test(unittest.TestCase):
+
+    def test(self):
+        self.assertEqual(1, 1)
